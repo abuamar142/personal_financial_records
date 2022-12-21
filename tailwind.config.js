133146a276aa4ec1eea093e6b3d0274745b164da
abuamar142 <amarjabon130@gmail.com>
2022-12-21 11:31:27 +0700

    ganti ke pallete warna pada file login, register, dashboard, dan edit profil

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          transparent : 'transparent',
+          current : 'currentColor',
+          primary : '#FFFBEB',
+          secondary : '#263159',
+          'text-warning' : ''
+        },
+      },
+    },
+    plugins: [],
+}
